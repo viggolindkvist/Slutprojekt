5dd1b0179e54939658c05b4d25d728ce54416e52
Viggo Lindkvist <viggo.lindkvist@skola.taby.se>
2022-05-04 14:39:39 +0200

    progress

--- finalSeats.js
+++ finalSeats.js
@@ -1,3 +1,5 @@
 const seatsSpan = document.querySelector("#seats");
+const priceSpan = document.querySelector("#price");
 
 seatsSpan.innerText = localStorage.getItem("seats");
+priceSpan.innerText = parseInt(localStorage.getItem("seats")) * 10;
